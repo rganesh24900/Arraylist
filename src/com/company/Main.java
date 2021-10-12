823b084e83b53573692e5850d0101d35e23af991
Ganesh <rganesh802@gmail.com>
2021-10-12 21:13:32 +0530

    Arraylist problem

---
+++ src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        ArrayList<Object> l1 = new ArrayList<Object>();
+        Test t1 =new Test("Ganesh","FullTime");
+        Test t2 =new Test("Tommy","PartTime");
+        Test t3 = new Test("Bucky","FullTime");
+        l1.add(t1);
+        l1.add(t2);
+        l1.add(t3);
+
+        ArrayList<Integer> i1 = new ArrayList<Integer>();
+
+        i1.add(4);
+        i1.add(6);
+        i1.add(4);
+        System.out.println(l1);
+        System.out.println("Replacing the object t2 with value 8: ");
+        l1.set(1,8);
+        System.out.println(l1);
+
+
+        System.out.println("Adding both the arraylist:");
+        l1.addAll(i1);
+
+        System.out.println("Removing elements of i1 from l1");
+        l1.removeAll(i1);
+
+            System.out.println(l1);
+
+
+            System.out.println("Traversing the elements in arraylist using iterator:");
+            Iterator itr = l1.iterator();
+            while (itr.hasNext()){
+                System.out.println(itr.next());
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+
---
+++ src/com/company/Test.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Test {
+    String name;
+    String empType;
+    Test(String name,String empType){
+        this.name =name;
+        this.empType=empType;
+    }
+}
